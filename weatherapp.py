cd5e3a6665af0a31e72d4b2c7ede96a9a1568f20
Evgeny_Skotarenko <jeny797@gmail.com>
2018-11-15 20:04:30 +0200

    modul_4_task_2_min

--- weatherapp.py
+++ weatherapp.py
@@ -2,6 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+# the task 1
+# Змерджити гілку m04_ls1 в develop та видалити
+# complete
+
+# the task 2
+# Створити гілку m04_ls2
+# complete
+
+# the task 3
 url = 'https://www.accuweather.com/uk/ua/kyiv/324505/weather-forecast/324505'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'}
@@ -9,7 +18,6 @@ headers = {
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, "html.parser")
 
-# the task 3
 temprege = soup.find('span', 'large-temp')
 place = soup.find('span', 'current-city')
 print('The temprege in {} is {}'.format(place.text[:place.text.find(',')], temprege.text))
