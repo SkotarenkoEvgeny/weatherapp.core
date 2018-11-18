d67df96fd5a84656e80e050f382b0bdb9152cb1e
Evgeny_Skotarenko <jeny797@gmail.com>
2018-11-18 22:00:07 +0200

    Tasks 1, 4, 5

--- name in encode.py
+++ name in encode.py
@@ -1,6 +1,26 @@
 #printing name in encode
 
 name = input('Input your name ')
+#name = 'Тарас'
 name_encode = name.encode('utf-8')
-print(name_encode)
-print(name_encode.decode(encoding='utf-8'))
+print('Unicode\n')
+print(name_encode, "\n")
+
+
+'''
+Написати програму для перетворення імені користувача у unicode code points
+Наприклад: Тарас → '422430440430441'
+'''
+
+print('hexadecimal\n')
+print(''.join(format(ord(x), 'x') for x in name), "\n")
+
+
+'''
+Написати програму для перетворення імені користувача в бінарне представлення.
+Наприклад:
+Тарас → '1000010001010000110000100010000001000011000010001000001'
+'''
+
+print('binary\n')
+print(''.join(format(ord(x), 'b') for x in name), "\n")
