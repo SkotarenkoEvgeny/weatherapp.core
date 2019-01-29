b5059e981ae53b98f6373d00ec67990cb25dfc94
Evgeny_Skotarenko <jeny797@gmail.com>
2019-01-29 20:41:35 +0200

    add command.py file to GIT

---
+++ weatherapp/core/abstract/command.py
@@ -0,0 +1,22 @@
+import abc
+import argparse
+
+
+class Command(abc.ABC):
+    '''Base class for commands'''
+
+    call = 0
+
+    def __init__(self, site_name):
+        self.site_name = site_name
+
+    @staticmethod
+    def get_parser():
+        parser = argparse.ArgumentParser()
+        return parser
+
+    @abc.abstractmethod
+    def run(self):
+        '''
+        Invoiced by application when the command is run
+        '''
